56606cff001682a7f0fd193f467baf36b3669988
wkx <wangkx0227@163.com>
2024-08-21 22:39:41 +0800

    '更新'

--- src/views/UserDropdown/UserInfoView.vue
+++ src/views/UserDropdown/UserInfoView.vue
@@ -1,138 +1,152 @@
 <template>
   <div class="userinfo" v-loading="loading">
-    <el-descriptions title="用户基本信息" direction="vertical" :column="4" border>
-      <el-descriptions-item :span="1">
-        <template slot="label">
-          <i class="el-icon-user"></i>
-          中文名称
-        </template>
-        {{ user_info_form.user_name }}
-      </el-descriptions-item>
-      <el-descriptions-item :span="1">
-        <template slot="label">
-          <i class="el-icon-suitcase"></i>
-          职位
-        </template>
-        {{ user_info_form.position }}
-      </el-descriptions-item>
-      <el-descriptions-item :span="1">
-        <template slot="label">
-          <i class="el-icon-user"></i>
-          账户名称
-        </template>
-        {{ user_info_form.login_name }}
-      </el-descriptions-item>
-      <el-descriptions-item :span="1">
-        <template slot="label">
-          <i class="el-icon-office-building"></i>
-          账户所属公司
-        </template>
-        {{ user_info_form.firm }}
-      </el-descriptions-item>
-      <el-descriptions-item :span="1">
-        <template slot="label">
-          <i class="el-icon-shopping-bag-2"></i>
-          所属部门
-        </template>
-        {{ user_info_form.department }}
-      </el-descriptions-item>
-      <el-descriptions-item :span="1">
-        <template slot="label">
-          <i class="el-icon-bank-card"></i>
-          账户唯一ID
-        </template>
-        {{ user_info_form.prefix }}-{{ user_info_form.user_id }}
-      </el-descriptions-item>
-      <el-descriptions-item :span="1">
-        <template slot="label">
-          <i class="el-icon-chat-line-round"></i>
-          邮箱
-        </template>
-        <span v-if="user_info_form.email">
+    <el-card class="box-card">
+      <el-descriptions title="用户基本信息" direction="vertical" size="medium" :column="4" border>
+        <el-descriptions-item :span="1">
+          <template slot="label">
+            <i class="el-icon-user"></i>
+            中文名称
+          </template>
+          {{ user_info_form.user_name }}
+        </el-descriptions-item>
+        <el-descriptions-item :span="1">
+          <template slot="label">
+            <i class="el-icon-suitcase"></i>
+            职位
+          </template>
+          {{ user_info_form.position }}
+        </el-descriptions-item>
+        <el-descriptions-item :span="1">
+          <template slot="label">
+            <i class="el-icon-user"></i>
+            账户名称
+          </template>
+          {{ user_info_form.login_name }}
+        </el-descriptions-item>
+        <el-descriptions-item :span="1">
+          <template slot="label">
+            <i class="el-icon-office-building"></i>
+            账户所属公司
+          </template>
+          {{ user_info_form.firm }}
+        </el-descriptions-item>
+        <el-descriptions-item :span="1">
+          <template slot="label">
+            <i class="el-icon-shopping-bag-2"></i>
+            所属部门
+          </template>
+          {{ user_info_form.department }}
+        </el-descriptions-item>
+        <el-descriptions-item :span="2">
+          <template slot="label">
+            <i class="el-icon-time"></i>
+            账户创建时间
+          </template>
+          {{ user_info_form.create_date }}
+        </el-descriptions-item>
+        <el-descriptions-item :span="1">
+          <template slot="label">
+            <i class="el-icon-bank-card"></i>
+            账户唯一ID
+          </template>
+          {{ user_info_form.prefix }}-{{ user_info_form.user_id }}
+        </el-descriptions-item>
+        <el-descriptions-item :span="1">
+          <template slot="label">
+            <i class="el-icon-chat-line-round"></i>
+            邮箱
+          </template>
+          <span v-if="user_info_form.email">
           {{ user_info_form.email }}
         </span>
-        <span v-else>
+          <span v-else>
           无邮箱
         </span>
-        <el-button size="mini" type="text" @click="emailShowDialog" style="float: right">修改邮箱</el-button>
-      </el-descriptions-item>
-      <el-descriptions-item :span="1">
-        <template slot="label">
-          <i class="el-icon-view"></i>
-          密码
-        </template>
-        ******
-        <el-button size="mini" type="text" @click="pwdShowDialog" style="float: right">修改密码
-        </el-button>
-      </el-descriptions-item>
-      <el-descriptions-item :span="2">
-        <template slot="label">
-          <i class="el-icon-time"></i>
-          账户创建时间
-        </template>
-        {{ user_info_form.create_date }}
-      </el-descriptions-item>
-      <el-descriptions-item :span="2">
-        <template slot="label">
-          <i class="el-icon-time"></i>
-          账户修改时间
-        </template>
-        {{ user_info_form.update_date }}
-      </el-descriptions-item>
-      <el-descriptions-item :span="1">
-        <template slot="label">
-          <i class="el-icon-edit"></i>
-          延期审核主管
-        </template>
-        罗力
-      </el-descriptions-item>
-      <el-descriptions-item :span="4">
-        <template slot="label">
-          <i class="el-icon-reading"></i>
-          基础功能
-        </template>
-        {{ user_info_form.foundation_api }}
-      </el-descriptions-item>
-      <el-descriptions-item :span="4">
-        <template slot="label">
-          <i class="el-icon-reading"></i>
-          功能功能
-        </template>
-        {{ user_info_form.function_api }}
-      </el-descriptions-item>
-    </el-descriptions>
-    <div class="dialog">
-      <el-dialog title="修改密码" :visible.sync="pwdDialogVisible" width="25%">
-        <el-form :model="form" :rules="rules" ref="form">
-          <el-form-item label="新密码" prop="password">
-            <el-input v-model="form.password" show-password></el-input>
-          </el-form-item>
-          <el-form-item label="确认密码" prop="repassword">
-            <el-input v-model="form.repassword" show-password></el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="submitForm('form')" style="float: right">提交修改
-            </el-button>
-          </el-form-item>
-        </el-form>
-      </el-dialog>
-    </div>
-    <div class="dialog">
-      <el-dialog title="修改邮箱" :visible.sync="emailDialogVisible" width="25%">
-        <el-form :model="emailForm"  ref="emailForm">
-          <el-form-item label="新邮箱" prop="email" :rules="[
+          <el-button size="mini" type="text" @click="emailShowDialog" style="float: right">修改邮箱</el-button>
+        </el-descriptions-item>
+        <el-descriptions-item :span="2">
+          <template slot="label">
+            <i class="el-icon-time"></i>
+            账户修改时间
+          </template>
+          {{ user_info_form.update_date }}
+        </el-descriptions-item>
+        <el-descriptions-item :span="1">
+          <template slot="label">
+            <i class="el-icon-view"></i>
+            密码
+          </template>
+          ******
+          <el-button size="mini" type="text" @click="pwdShowDialog" style="float: right">修改密码
+          </el-button>
+        </el-descriptions-item>
+        <el-descriptions-item :span="1">
+          <template slot="label">
+            <i class="el-icon-edit"></i>
+            延期审核主管
+          </template>
+          罗力
+        </el-descriptions-item>
+        <el-descriptions-item :span="1">
+          <template slot="label">
+            <i class="el-icon-set-up"></i>
+            是否开启中文账户名称登录
+          </template>
+          <el-switch
+              v-model="user_info_form.is_user_name_login"
+              @change="changeSwitch($event,user_info_form)"
+              active-text="开启"
+              inactive-text="关闭">
+          </el-switch>
+        </el-descriptions-item>
+        <el-descriptions-item :span="2">
+          <template slot="label">
+            <i class="el-icon-reading"></i>
+            基础功能
+          </template>
+          {{ user_info_form.foundation_api }}
+        </el-descriptions-item>
+        <el-descriptions-item :span="4">
+          <template slot="label">
+            <i class="el-icon-reading"></i>
+            功能功能
+          </template>
+          {{ user_info_form.function_api }}
+        </el-descriptions-item>
+      </el-descriptions>
+      <div class="dialog">
+        <el-dialog title="修改密码" :visible.sync="pwdDialogVisible" width="25%">
+          <el-form :model="form" :rules="rules" ref="form">
+            <el-form-item label="新密码" prop="password">
+              <el-input v-model="form.password" show-password></el-input>
+            </el-form-item>
+            <el-form-item label="确认密码" prop="repassword">
+              <el-input v-model="form.repassword" show-password></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm('form')" style="float: right">提交修改
+              </el-button>
+            </el-form-item>
+          </el-form>
+        </el-dialog>
+      </div>
+      <div class="dialog">
+        <el-dialog title="修改邮箱" :visible.sync="emailDialogVisible" width="25%">
+          <el-form :model="emailForm" ref="emailForm">
+            <el-form-item label="新邮箱" prop="email" :rules="[
           { required: true, message: '请输入邮箱地址', trigger: 'blur' },
           { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
           ]">
-            <el-input v-model="emailForm.email"></el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="submitEmailForm('emailForm')" style="float: right">提交修改
-            </el-button>
-          </el-form-item>
-        </el-form>
-      </el-dialog>
-    </div>
+              <el-input v-model="emailForm.email"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitEmailForm('emailForm')" style="float: right">提交修改
+              </el-button>
+            </el-form-item>
+          </el-form>
+        </el-dialog>
+      </div>
+    </el-card>
   </div>
 </template>
 
@@ -172,7 +186,7 @@ export default {
         repassword: "",
       },
       // 邮箱
-      emailForm:{
+      emailForm: {
         email: null,
       },
       emailDialogVisible: false,
@@ -186,7 +200,6 @@ export default {
   created() {
     this.loading = true;
     this.getUserInfo();
-
   },
   methods: {
     // 获取用户基本数据，无法获取延期审核人信息，与权限信息
@@ -258,7 +271,6 @@ export default {
             }, 1000);
           });
     },
-
     // 发送请求,修改密码
     updateEmail() {
       this.$http
@@ -287,20 +299,21 @@ export default {
         }
       });
     },
+    // 开关方法变动回调函数,修改是否开启中文名称登录功能
+    changeSwitch(newValue, row) {
+      const pk = row.id;
+      let is_user_name_login = newValue ? 1 : 0;
+      console.log(is_user_name_login)
+    }
   },
 };
 </script>
 
 <style>
-.userinfo{
-  width: 70%;
-  margin: 0 auto;
-}
+@import url("@/static/currency.css");
+
 @media screen and (max-width: 700px) {
-  .userinfo{
-    width: 100%;
-    margin: 0 auto;
-  }
+
   .userinfo .el-button--mini {
     font-size: 9px;
     padding: 5px 8px;
