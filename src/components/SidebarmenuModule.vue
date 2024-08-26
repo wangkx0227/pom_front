935ecb9ecbc3570038927b7f4e56a3e0c425c368
wkx <wangkx0227@163.com>
2024-08-26 22:37:00 +0800

    '更新'

--- src/components/SidebarmenuModule.vue
+++ src/components/SidebarmenuModule.vue
@@ -55,11 +55,9 @@
       </template>
       <el-menu-item index="/business_function/no_order_matter">非订单</el-menu-item>
       <el-menu-item index="/business_function/order_matter">订单类</el-menu-item>
-      <el-menu-item index="7-6">事务关闭</el-menu-item>
       <el-menu-item index="7-4">事务变更</el-menu-item>
       <el-menu-item index="/business_function/factory_bind_user">工厂绑定</el-menu-item>
       <el-menu-item index="/business_function/email">邮件功能</el-menu-item>
-
     </el-submenu>
   </el-menu>
 </template>
