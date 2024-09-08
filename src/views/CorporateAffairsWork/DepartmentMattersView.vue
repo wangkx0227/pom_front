7de8fcac95551ebbb8dd21c5a4f127aa463e0476
wkx <wangkx0227@163.com>
2024-09-08 22:41:48 +0800

    '更新'

--- src/views/CorporateAffairsWork/DepartmentMattersView.vue
+++ src/views/CorporateAffairsWork/DepartmentMattersView.vue
@@ -1,13 +1,33 @@
 <template>
-
+  <div class="order_info_list">
+    <el-card class="box-card">
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="部门事务" name="department">
+          <component :is="DepartmentComponent" v-if="activeName === 'department'"/>
+        </el-tab-pane>
+      </el-tabs>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-name: "SupervisoryWorkView"
-}
+  name: "DepartmentView",
+  data() {
+    return {
+      activeName: "department",
+      // 使用懒加载形式加载组件
+      DepartmentComponent: () => import("@/components/DepartmentComponent/DepartmentWorkModule"),
+    };
+  },
+  methods: {
+    handleClick(tab) {
+      this.activeName = tab.name;
+    },
+  },
+};
 </script>
 
 <style scoped>
-
+@import url("@/static/convention.css");
 </style>
