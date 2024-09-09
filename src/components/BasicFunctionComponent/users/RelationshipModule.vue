954ff377532d1023e5cda01ca9a13971ed782d82
wkx <wangkx0227@163.com>
2024-09-09 23:12:46 +0800

    '更新'

--- src/components/BasicFunctionComponent/users/RelationshipModule.vue
+++ src/components/BasicFunctionComponent/users/RelationshipModule.vue
@@ -4,14 +4,14 @@
       <h3>经理</h3>
       <el-divider>经理</el-divider>
       <el-table
-          :data="tableData"
+          :data="user_data_manage_list"
           style="width: 100%" height="600">
         <el-table-column
             prop="index"
             label="#" align="center">
         </el-table-column>
         <el-table-column
-            prop="name"
+            prop="user_name"
             label="姓名" align="center">
         </el-table-column>
         <el-table-column
@@ -24,21 +24,19 @@
       <h3>组长</h3>
       <el-divider>组长</el-divider>
       <el-table
-          ref="multipleTable"
-          :data="tableData"
+          :data="user_data_group_leader_list"
           tooltip-effect="dark"
           style="width: 100%"
           @selection-change="handleSelectionChange">
         <el-table-column
-            type="selection"
-            >
+            type="selection">
         </el-table-column>
         <el-table-column
             prop="index"
             label="#" align="center">
         </el-table-column>
         <el-table-column
-            prop="name"
+            prop="user_name"
             label="姓名" align="center">
         </el-table-column>
         <el-table-column
@@ -51,8 +49,7 @@
       <h3>组员</h3>
       <el-divider>组员</el-divider>
       <el-table
-          ref="multipleTable"
-          :data="tableData"
+          :data="user_data_member_list"
           tooltip-effect="dark"
           style="width: 100%"
           @selection-change="handleSelectionChange">
@@ -65,7 +62,7 @@
             label="#" align="center">
         </el-table-column>
         <el-table-column
-            prop="name"
+            prop="user_name"
             label="姓名" align="center">
         </el-table-column>
         <el-table-column
@@ -83,19 +80,14 @@ export default {
   data() {
     return {
       loading:false,
-      tableData: [{
-        index: '1',
-        name: '王小虎1',
-        department: '财务部门'
-      }, {
-        index: '2',
-        name: '王小虎2',
-        department: 'FMR'
-      }]
+      user_data_manage_list:[],
+      user_data_group_leader_list:[],
+      user_data_member_list:[],
     };
   },
   created() {
-
+    this.loading = true;
+    this.getRelationshipData();
   },
   methods: {
     toggleSelection(rows) {
@@ -109,7 +101,29 @@ export default {
     },
     handleSelectionChange(val) {
       this.multipleSelection = val;
-    }
+    },
+    getRelationshipData(){
+
+      this.$http
+          .get("users/user_relationship/")
+          .then((res) => {
+            let data = res.data;
+            if (data.code === 200) {
+              console.log(data)
+              this.user_data_manage_list = data.data.user_data_manage_list;
+              this.user_data_group_leader_list = data.data.user_data_group_leader_list;
+              this.user_data_member_list = data.data.user_data_member_list;
+            } else {
+              this.$message.error(data.message);
+            }
+          })
+          .catch((error) => {
+            this.$message.error(error.message);
+          })
+          .finally(() => {
+            this.loading = false;
+          });
+    },
   }
 
 };
