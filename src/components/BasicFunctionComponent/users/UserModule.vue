35c2df4940111192560eafab09569b64b95637ba
wkx <wangkx0227@163.com>
2024-09-11 22:58:25 +0800

    '提交'

--- src/components/BasicFunctionComponent/users/UserModule.vue
+++ src/components/BasicFunctionComponent/users/UserModule.vue
@@ -128,7 +128,7 @@
         <el-table-column label="归属部门" align="center" width="180">
           <template v-slot="{ row }">
             <div class="tag-group" v-if="!row.editable">
-              <el-tag style="margin-right: 2px" type="success" effect="plain">
+              <el-tag style="margin-right: 2px" type="success" effect="plain" v-if="row.department">
                 {{ row.department }}
               </el-tag>
             </div>
@@ -141,7 +141,7 @@
         <el-table-column label="职务/岗位" align="center" width="180">
           <template v-slot="{ row }">
             <div class="tag-group" v-if="!row.editable">
-              <el-tag type="position" effect="plain">
+              <el-tag type="position" effect="plain" v-if="row.position">
                 {{ row.position }}
               </el-tag>
             </div>
